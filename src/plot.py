d77a4a13c23d9a95741620ae5dd10b72f68399b9
unknown <snelson01010@gmail.com>
2023-03-16 13:06:17 +0000

    futher modified the plots

--- src/plot.py
+++ src/plot.py
@@ -39,34 +39,51 @@ def plot_chase_graph(csv_file_name: str) -> None:
     generations = range(1, len(predator_average_fitness) + 1)
 
     # Create a figure and two axes objects
-    fig, ax1 = plt.subplots(figsize=(12, 8))
+    fig, ax1 = plt.subplots(figsize=(18, 8))
     ax2 = ax1.twinx()
 
     # Plot the first set of data on the first y-axis
-    ax1.plot(generations, prey_average_fitness, label='prey Average fitness', color='blue')
-    ax1.plot(generations, prey_best_fitness, label='Prey Best fitness', color='red')
+    ax1.plot(generations, prey_average_fitness, label='Prey Average fitness', color='blue')
+    ax1.plot(generations, prey_best_fitness, label='Prey Best fitness', color='red', linestyle='dotted')
 
     ax1.set_xlabel('Generations')
     ax1.set_ylabel('Prey Fitness')
-    # ax1.tick_params('y', colors='b')
+
+    ax1.set_ylim([0, 1])
+
+    ax1.tick_params('y', colors='blue')
 
     # Plot the second set of data on the second y-axis
     ax2.plot(generations, predator_average_fitness, label='Predator Average fitness', color='green')
-    ax2.plot(generations, predator_best_fitness, label='Predator Best fitness', color='purple')
+    ax2.plot(generations, predator_best_fitness, label='Predator Best fitness', color='purple', linestyle='dotted')
 
     ax2.set_ylabel('Predator Fitness')
-    # ax2.tick_params('y', colors='r')
+
+    ax2.set_ylim([0, 30])
+
+    ax2.tick_params('y', colors='green')
 
     # Add legends for the two plots
     lines1, labels1 = ax1.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
-    ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper center', ncol=4, frameon=False)
+    ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper center', bbox_to_anchor=(0.5, 1.07), ncol=4, frameon=False, prop={'size': 14})
 
-    
-    plt.title('Fitness over generations')
+    plt.title('Fitness over generations', y=1.08)
 
     file_name = csv_file_name[:-4]
 
+    ax1.margins(x=0)
+    ax2.margins(x=0)
+
+    ax1.title.set_fontsize(18)
+    ax1.xaxis.label.set_fontsize(18)
+    ax1.yaxis.label.set_fontsize(18)
+
+
+    ax2.title.set_fontsize(18)
+    ax2.yaxis.label.set_fontsize(18)
+ 
+ 
     #save graph to directory
     plt.savefig('plots/{}.png'.format(file_name))
 
